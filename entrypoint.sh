680ad7d0a824dcda29d3884c22624ffff5382aeb
Jaakko Koponen <jaakko.koponen@solita.fi>
2023-05-31 14:02:50 +0300

    Add extra logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+whoami
+ls -lah $GITHUB_ACTION_PATH/dist/
+ls -lah $GITHUB_ACTION_PATH/dist/$(echo "$OS" | tr "[:upper:]" "[:lower:]")
+
 # Select right go binary for runner os
 $GITHUB_ACTION_PATH/dist/$(echo "$OS" | tr "[:upper:]" "[:lower:]")
